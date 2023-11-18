40d5ce47a5f4d5bc029d4c37539f2e118e47521e
yangtzeblues <anakin308@gmail.com>
2023-11-19 02:00:09 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Anakin Cheung on 2023/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
